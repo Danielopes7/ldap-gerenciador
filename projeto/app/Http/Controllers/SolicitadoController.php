a967133a3ed365fa0d19c75794cf738ae010fd0a
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-04-17 11:28:23 -0300

    criando model/migrate/controller para solicitacao de criacao de usuarios

---
+++ projeto/app/Http/Controllers/SolicitadoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Solicitado;
+use Illuminate\Http\Request;
+
+class SolicitadoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Solicitado  $solicitado
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Solicitado $solicitado)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Solicitado  $solicitado
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Solicitado $solicitado)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Solicitado  $solicitado
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Solicitado $solicitado)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Solicitado  $solicitado
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Solicitado $solicitado)
+    {
+        //
+    }
+}
